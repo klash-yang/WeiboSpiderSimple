6111383a93e715842e061791a47e41c854737f12
Yang Cheng <poloyc@126.com>
2019-03-20 00:26:12 +0800

    Add Column

--- inscrawler/test.py
+++ inscrawler/test.py
@@ -1,16 +1,18 @@
-from python_wpapi import python_wpapi
+import requests
 
 
-# @patch.object(python_wpapi.WpApi, '_request')
-def test_delete_media(api):
-    api.delete_media(id=434)
-    # mock.assert_called_with('http://base.url/wp-json/wp/v2/media/7',
-    #     force=False,
-    #     method='DELETE')
+def upload(file_name):
+    url = 'http://www.freebuf.com/buf/plugins/ueditor/ueditor/php/imageUp.php&post_id='
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+    cookies = {
+        '3cb185a485c81b23211eb80b75a406fd': '1524312580',
+        'PHPSESSID': 'lrps8el9u799le2agl56hhqlf0'}
+    r = requests.post(url, headers=headers, cookies=cookies,
+                      files={'upfile': open(file_name, 'rb')}, )
+
+    return r
 
 
-api = python_wpapi.WpApi('https://www.onedaycp.com', user='poloyc', password='Suckerlove5')
-test_delete_media(api)
-print(00000)
-
-# test_delete_media(api)
+a = upload('./data/edcee3000/Bu_N2oRnLtg/image.jpg')
+print(a)
--- inscrawler/test2.py
+++ inscrawler/test2.py
@@ -1,25 +1,25 @@
 import requests
 import json
 
-# file = open('../data/edcee3000/Bu6A4AKHoZt/image.jpg', 'rb')
 
-# https://api.images.ac.cn/?type=multipart\
-file = open('../data/pic5.jpg', 'rb')
+
+url = 'https://apis.yum6.cn/api/5bd44dc94bcfc?token=f07b711396f9a05bc7129c4507fb65c5'
+
+
+file = open('./data/edcee3000/Bu_N2oRnLtg/image.jpg', 'rb')
 # file_params = {'file': ('pic.jpg', file, 'image/jpeg')}
 file_params = {'file': ('pic.jpg', file, 'image/jpeg')}
 
 data = {
-    'ssl': True,
     'Accept': 'application/json, text/javascript, */*; q=0.01',
-
 }
 
 r = requests.post(
-    url='https://apis.yum6.cn/api/5bd44dc94bcfc?token=f07b711396f9a05bc7129c4507fb65c5',
+    url=url,
     data=data,
     files=file_params,
 
 )
 
 a = json.loads(r.content)
-print()
+print(a['data']['url'])
