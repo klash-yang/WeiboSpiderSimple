2d7908b897e5b1808262cf929e05553d65343602
Yang Cheng <poloyc@126.com>
2019-03-27 23:56:38 +0800

    Add Column

---
+++ inscrawler/run_once.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+import schedule
+import inscrawler.crawl as crawl
+import inscrawler.utils.post_wordpress as post_wordpress
+
+
+def job():
+    crawl.scrap()
+    post_wordpress.post()
+
+
+job()
